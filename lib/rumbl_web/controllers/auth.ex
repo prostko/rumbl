ab30721bf5a4aacad23ea743eeb079e97b8b2d19
prostko <eprostko@optoro.com>
2019-06-01 18:08:52 -0400

    created user gets logged in

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,6 +1,5 @@
 defmodule Rumbl.Auth do
   import Plug.Conn
-  alias Rumbl.Repo
   alias Rumbl.User
 
   def init(opts) do
@@ -11,5 +10,12 @@ defmodule Rumbl.Auth do
     user_id = get_session(conn, :user_id)
     user = user_id && repo.get(User, user_id)
     assign(conn, :current_user, user)
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
   end
 end
