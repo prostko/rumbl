13ee194da8d446177d7d6c497ba5a1b77aa103d7
prostko <eprostko@optoro.com>
2019-06-01 14:31:00 -0400

    initial commit, up to page 79

---
+++ priv/repo/migrations/20190526171551_create_user.exs
@@ -0,0 +1,14 @@
+defmodule Rumbl.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:user) do
+      add :name, :string
+      add :username, :string
+      add :password_hash, :string
+
+      timestamps()
+    end
+
+  end
+end
