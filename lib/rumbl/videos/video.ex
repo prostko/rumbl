86c807fe508f1f4682a380c866fd3ea88ffd952c
Eric Prostko <ericprostko9@gmail.com>
2019-07-08 12:41:29 -0600

    added slug to video, slugify on update

--- lib/rumbl/videos/video.ex
+++ lib/rumbl/videos/video.ex
@@ -6,6 +6,7 @@ defmodule Rumbl.Videos.Video do
     field :descriptions, :string
     field :title, :string
     field :url, :string
+    field :slug, :string
 
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Categories.Category
@@ -18,7 +19,20 @@ defmodule Rumbl.Videos.Video do
     video
     |> cast(attrs, [:url, :title, :descriptions, :category_id])
     |> validate_required([:url, :title, :descriptions])
+    |> slugify_title()
     |> foreign_key_constraint(:category_id)
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else 
+      changeset
+    end
+  end
+
+  defp slugify(title) do
+    title |> String.downcase |> String.replace(~r/[\W-]+/u, "-")
   end
 end
