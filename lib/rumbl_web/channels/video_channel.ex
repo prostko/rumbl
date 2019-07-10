f32cfab65777e5dbdca6e78b23b48c57e67687d6
Eric Prostko <ericprostko9@gmail.com>
2019-07-10 05:04:31 -0600

    comments on channels

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -5,7 +5,11 @@ defmodule RumblWeb.VideoChannel do
     {:ok, socket}
   end
 
+  # The handle_in method handles ALL incoming messages to a channel. 
+  # It waits until it sees a message that matches the defined message here.
   def handle_in("new_annotation", params, socket) do
+
+    # broadcast sends this payload to all of the channels open on this topic. 
     broadcast!(socket, "new_annotation", %{
       user: %{username: "anon"},
       body: params["body"],
